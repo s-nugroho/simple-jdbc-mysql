4c40e30cd263e9f0b3f1084c8edc0405ea6f81bd
s.nugroho <seftianhd@gmail.com>
2024-08-19 21:25:36 +0700

    - add config build name for result build

---
+++ src/main/resources/db/populator.sql
@@ -0,0 +1,8 @@
+# CREATE TABLE IF NOT EXISTS core_test.role
+# (
+#     id          SERIAL PRIMARY KEY,
+#     name        VARCHAR(255) NOT NULL UNIQUE,
+#     description VARCHAR(255) NOT NULL
+# );
+#
+# CREATE TABLE IF NOT EXISTS
